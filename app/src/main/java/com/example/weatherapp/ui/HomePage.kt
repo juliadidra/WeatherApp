3e5c98fe6128efcdc85e5af5d8d7c4f47a0e78f1
juliadidra <juliadidra22@gmail.com>
2025-06-19 20:47:54 -0300

    feat: Pratica 02 - Parte 1

--- app/src/main/java/com/example/weatherapp/ui/HomePage.kt
+++ app/src/main/java/com/example/weatherapp/ui/HomePage.kt
@@ -1,15 +1,9 @@
-package com.example.weatherapp
+package com.example.weatherapp.ui
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -17,28 +11,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
-import com.example.weatherapp.ui.HomePage
-import com.example.weatherapp.ui.theme.WeatherAppTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            WeatherAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    HomePage(modifier = Modifier.padding(innerPadding))
-                }
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true)
 @Composable
-fun HomePage() {
+fun HomePage(modifier: Modifier) {
     Column(
         modifier = Modifier.fillMaxSize()
             .background(Color.Blue)
@@ -54,4 +30,3 @@ fun HomePage() {
         )
     }
 }
-
