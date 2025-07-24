6200f647a9e1d4509be9a588f1301f2d61add743
juliadidra <juliadidra22@gmail.com>
2025-07-24 19:34:17 -0300

    feat: Pratica 06 - Parte 1 -  Preparação para usar o Firebase Firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation(libs.androidx.material3)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
