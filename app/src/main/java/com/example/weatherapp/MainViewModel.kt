3973b790179b77db9a64a75cbb78e9d2c603efa9
juliadidra <juliadidra22@gmail.com>
2025-07-24 20:07:59 -0300

    feat: Pratica 06 - Parte 2 -  Criando as classes do banco de dados

--- app/src/main/java/com/example/weatherapp/MainViewModel.kt
+++ app/src/main/java/com/example/weatherapp/MainViewModel.kt
@@ -1,12 +1,17 @@
 package com.example.weatherapp
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
 import com.example.weatherapp.ui.model.City
+import com.example.weatherapp.ui.model.User
 import com.google.android.gms.maps.model.LatLng
 
 class MainViewModel : ViewModel() {
     private val _cities = getCities().toMutableStateList()
+    private val _user = mutableStateOf<User?> (null)
+    val user : User?
+        get() = _user.value
     val cities
         get() = _cities.toList()
     fun remove(city: City) {
