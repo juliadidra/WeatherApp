30df9a9bf464a1dec7a769491724da27aa9b836e
juliadidra <juliadidra22@gmail.com>
2025-08-14 18:18:38 -0300

    fix: Pratica 08 - Parte 3: ajeitando formatação

--- app/src/main/java/com/example/weatherapp/api/WeatherService.kt
+++ app/src/main/java/com/example/weatherapp/api/WeatherService.kt
@@ -12,95 +12,55 @@ class WeatherService {
     private var weatherAPI: WeatherServiceAPI
 
     init {
-
         val retrofitAPI = Retrofit.Builder().baseUrl(WeatherServiceAPI.BASE_URL)
-
             .addConverterFactory(GsonConverterFactory.create()).build()
-
         weatherAPI = retrofitAPI.create(WeatherServiceAPI::class.java)
-
     }
 
     fun getName(lat: Double, lng: Double, onResponse : (String?) -> Unit ) {
-
         search("$lat,$lng") { loc -> onResponse (loc?.name) }
-
     }
 
     fun getLocation(name: String, onResponse: (lat:Double?, long:Double?) -> Unit) {
-
         search(name) { loc -> onResponse (loc?.lat, loc?.lon) }
-
     }
 
     private fun search(query: String, onResponse : (APILocation?) -> Unit) {
-
         val call: Call<List<APILocation>?> = weatherAPI.search(query)
-
         call.enqueue(object : Callback<List<APILocation>?> {
-
             override fun onResponse(call: Call<List<APILocation>?>,
-
                                     response: Response<List<APILocation>?>
-
             ) {
-
                 onResponse(response.body()?.let {if (it.isNotEmpty()) it[0] else null})
-
             }
 
             override fun onFailure(call: Call<List<APILocation>?>,t: Throwable) {
-
                 Log.w("WeatherApp WARNING", "" + t.message)
-
                 onResponse(null)
-
             }
-
         })
-
     }
 
     private fun <T> enqueue(call : Call<T?>, onResponse : ((T?) -> Unit)? = null){
-
         call.enqueue(object : Callback<T?> {
-
             override fun onResponse(call: Call<T?>, response: Response<T?>) {
-
                 val obj: T? = response.body()
-
                 onResponse?.invoke(obj)
-
             }
 
             override fun onFailure(call: Call<T?>, t: Throwable) {
-
                 Log.w("WeatherApp WARNING", "" + t.message)
-
             }
-
         })
-
     }
 
     fun getWeather(name: String, onResponse: (APICurrentWeather?) -> Unit){
-
         val call: Call<APICurrentWeather?> = weatherAPI.weather(name)
-
         enqueue(call) { onResponse.invoke(it) }
-
     }
-
 
     fun getForecast(name: String, onResponse : (APIWeatherForecast?) -> Unit) {
-
-
         val call: Call<APIWeatherForecast?> = weatherAPI.forecast(name)
-
-
         enqueue(call) { onResponse.invoke(it) }
-
-
     }
-
 }
